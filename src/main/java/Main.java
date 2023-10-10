fcbe498c546b711cf2d44e056fd6217a38acf5e4
vsvarost <vsvarost@gmail.com>
2023-10-10 23:17:01 +0300

    Task done.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,11 @@
+import java.io.FileWriter;
 import java.util.Scanner;
 
 public class Main {
+    public static ToyStore toyStore = new ToyStore();
+
     public static void main(String[] args) {
-        ToyStore toyStore = new ToyStore();
+
         toyStore.putToy(new Toy(1, "конструктор", 2));
         toyStore.putToy(new Toy(1, "робот", 2));
         toyStore.putToy(new Toy(1, "кукла", 6));
@@ -20,11 +23,32 @@ public class Main {
             int choice = scanner.nextInt();
             switch (choice) {
                 case 1: addToy();
+                case 2: raffle();
+                case 3: break;
             }
         }
     }
 
-    private static void addToy() {
+    private static void raffle() {
+        Toy toy = toyStore.getToy();
+        System.out.printf("Разыграна игрушка \"%s\".\n", toy.getName());
+    }
 
+    public static void addToy() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите ID игрушки:");
+        while (!scanner.hasNextInt()) {
+            System.out.println("Введено некорректное значение ID. Попробуйте еще раз.");
+        }
+        int id = scanner.nextInt();
+        System.out.println("Введите название игрушки: ");
+        String name = scanner.next();
+        System.out.println("Введите вес игрушки: ");
+        while (!scanner.hasNextInt()) {
+            System.out.println("Введено некорректное значение. Попробуйте еще раз.");
+        }
+        int weight = scanner.nextInt();
+
+        toyStore.putToy(new Toy(id, name, weight));
     }
 }
